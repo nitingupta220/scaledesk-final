4bb279e171c76ea0afd8caf9c46a4d51148db259
Neeraj <nkneeraj99@gmail.com>
2017-03-20 18:46:53 +0530

    created all sections with data

--- landing-page/js/main.js
+++ landing-page/js/main.js
@@ -1,3 +1,13 @@
 /**
  * Created by neerajsingh on 17/03/17.
  */
+
+$(window).scroll(function(event) {
+    var yOffset = window.pageYOffset;
+    var breakpoint = 550;
+    if (yOffset > breakpoint) {
+        $(".custom-nav").addClass('active');
+    } else {
+        $(".custom-nav").removeClass('active');
+    }
+});
