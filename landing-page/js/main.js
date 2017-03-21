ab5edf0ea017ea495f55601d04a655f89bac30a8
Neeraj <nkneeraj99@gmail.com>
2017-03-21 18:09:06 +0530

    added gallery image section

--- landing-page/js/main.js
+++ landing-page/js/main.js
@@ -2,12 +2,53 @@
  * Created by neerajsingh on 17/03/17.
  */
 
-$(window).scroll(function(event) {
-    var yOffset = window.pageYOffset;
-    var breakpoint = 550;
-    if (yOffset > breakpoint) {
-        $(".custom-nav").addClass('active');
-    } else {
-        $(".custom-nav").removeClass('active');
-    }
+//$(window).scroll(function(event) {
+//    var yOffset = window.pageYOffset;
+//    var breakpoint = 550;
+//    if (yOffset > breakpoint) {
+//        $(".custom-nav").addClass('active');
+//    } else {
+//        $(".custom-nav").removeClass('active');
+//    }
+//});
+//
+//
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
 });
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('custom-nav').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('custom-nav');
+        }
+    }
+
+    lastScrollTop = st;
+}
