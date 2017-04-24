0c54ec18033cc361756fb471e4054e049a68e3fa
Neeraj <nkneeraj99@gmail.com>
2017-04-24 17:48:57 +0530

    added new full screen menu

--- js/main.js
+++ js/main.js
@@ -9,3 +9,55 @@ $("input[type ='text']").click(function(){
 });
 
 //Ends here//
+
+
+//Navigation//
+
+$(function(){
+    $('.n-toggle').on('click', function(){
+        $('.off-canvas').toggleClass('open');
+    });
+
+    if ($('.off-canvas').hasClass('open')){
+        $(document).toggleClass('open');
+
+
+    }
+
+
+});
+
+
+//$(document).click(function(){
+//    if ($('off-canvas').hasClass('open')){
+//        $('off-canvas').removeClass('open');
+//
+//
+//    }
+//});
+
+
+//$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 500){
+            $("header").addClass('.fill');
+
+        }else {
+            $("header").removeClass('.fill');
+        }
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+//});
